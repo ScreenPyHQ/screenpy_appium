6ac569e0518a86ad6c4bbcbb7b13a10c880c3113
bandophahita <trenchrats@gmail.com>
2024-02-19 17:30:04 -0600

    converting __version__.py to use metadata from pyproject.toml

--- screenpy_appium/__version__.py
+++ screenpy_appium/__version__.py
@@ -1,29 +1,34 @@
-" .s5SSSs.                                                    .s5SSSs.         "
-#       SS. .s5SSSs.  .s5SSSs.  .s5SSSs.  .s5SSSs.  .s    s.        SS. .s5 s.
-# sS    `:;       SS.       SS.       SS.       SS.       SS. sS    S%S     SS.
-# SS        sS    `:; sS    S%S sS    `:; sS    `:; sSs.  S%S SS    S%S ssS SSS
-# `:;;;;.   SS        SS .sS;:' SSSs.     SSSs.     SS `S.S%S SS .sS::'  SSSSS
-#       ;;. SS        SS    ;,  SS        SS        SS  `sS%S SS          SSS
-#       `:; SS        SS    `:; SS        SS        SS    `:; SS          `:;
-# .,;   ;,. SS    ;,. SS    ;,. SS    ;,. SS    ;,. SS    ;,. SS          ;,.
-# `:;;;;;:' `:;;;;;:' `:    ;:' `:;;;;;:' `:;;;;;:' :;    ;:' `:          ;:'
-#
-#             .s5SSSs.
-#                   SS. .s5SSSs.  .s5SSSs.  s.  .s    s.  .s5ssSs.
-#             sS    S%S       SS.       SS. SS.       SS.    SS SS.
-#             SS    S%S sS    S%S sS    S%S S%S sS    S%S sS SS S%S
-#             SSSs. S%S SS .sS::' SS .sS::' S%S SS    S%S SS :; S%S
-#             SS    S%S SS        SS        S%S SS    S%S SS    S%S
-#             SS    `:; SS        SS        `:; SS    `:; SS    `:;
-#             SS    ;,. SS        SS        ;,. SS    ;,. SS    ;,.
-#             :;    ;:' `:        `:        ;:' `:;;;;;:' :;    ;:'
+"""
+    .s5SSSs.  .s5SSSs.  .s5SSSs.  .s5SSSs.  .s5SSSs.  .s    s.  .s5SSSs.  .s5 s.
+          SS.       SS.       SS.       SS.       SS.       SS.       SS.     SS.
+    sS    `:; sS    `:; sS    S%S sS    `:; sS    `:; sSs.  S%S sS    S%S ssS SSS
+    SS        SS        SS    S%S SS        SS        SS`S. S%S SS    S%S SSS SSS
+    `:;;;;.   SS        SS .sS;:' SSSs.     SSSs.     SS `S.S%S SS .sS::'  SSSSS
+          ;;. SS        SS    ;,  SS        SS        SS  `sS%S SS          SSS
+          `:; SS        SS    `:; SS        SS        SS    `:; SS          `:;
+    .,;   ;,. SS    ;,. SS    ;,. SS    ;,. SS    ;,. SS    ;,. SS          ;,.
+    `:;;;;;:' `:;;;;;:' `:    ;:' `:;;;;;:' `:;;;;;:' :;    ;:' `:          ;:'
 
+                .s5SSSs.  .s5SSSs.  .s5SSSs.  s.  .s    s.  .s5ssSs.
+                      SS.       SS.       SS. SS.       SS.    SS SS.
+                sS    S%S sS    S%S sS    S%S S%S sS    S%S sS SS S%S
+                SS    S%S SS    S%S SS    S%S S%S SS    S%S SS :; S%S
+                SSSs. S%S SS .sS::' SS .sS::' S%S SS    S%S SS    S%S
+                SS    S%S SS        SS        S%S SS    S%S SS    S%S
+                SS    `:; SS        SS        `:; SS    `:; SS    `:;
+                SS    ;,. SS        SS        ;,. SS    ;,. SS    ;,.
+                :;    ;:' `:        `:        ;:' `:;;;;;:' :;    ;:'
+"""
 
-__title__ = "screenpy_appium"
-__description__ = "ScreenPy extension that enables interacting with Appium."
-__url__ = "https://github.com/ScreenPyHQ/screenpy_appium"
-__version__ = "0.0.1"
-__author__ = "Perry Goy"
-__author_email__ = "perry.goy@gmail.com"
-__license__ = "MIT"
-__copyright__ = "Copyright (c) 2022-2023, Perry Goy"
+import importlib.metadata
+
+metadata = importlib.metadata.metadata("screenpy_adapter_allure")
+
+__title__ = metadata["Name"]
+__description__ = metadata["Summary"]
+__url__ = metadata["Home-page"]
+__version__ = metadata["Version"]
+__author__ = metadata["Author"]
+__author_email__ = metadata["Author-email"]
+__license__ = metadata["License"]
+__copyright__ = f"2019-2024 {__author__}"
